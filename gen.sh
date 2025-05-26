85c92a59e297d8bd177037ca987f78ec97c8ae5d
Marvin Borner <git@marvinborner.de>
2025-05-26 13:29:00 +0200

    Fix build script

--- gen.sh
+++ gen.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 mkdir -p build/ out/
-#rm -f out/* build/*
-rm -f build/*
+rm -f out/* build/*
 
 effekt.sh -o out/ --write-documentation $(find effekt/libraries/ -type f -name "*.effekt")
 cat out/*.json | jq -c -s . >build/full.json
